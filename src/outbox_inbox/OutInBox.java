35ed22373633d899c407dab05eef420989a8508c
chokrihamza <chokrihamza1994@gmail.com>
2021-08-29 15:03:33 +0200

    in out box

---
+++ src/outbox_inbox/OutInBox.java
@@ -0,0 +1,15 @@
+package outbox_inbox;
+
+public class OutInBox {
+public static void main(String[] args) {
+	
+	Integer number=10;
+	System.out.println(number.toString());
+	System.out.println(number.doubleValue());
+	
+}
+	
+	
+	
+	
+}
