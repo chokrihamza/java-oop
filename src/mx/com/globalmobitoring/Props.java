b9f151774d8f6a71aae2573c38268920b9ad69a2
chokrihamza <chokrihamza1994@gmail.com>
2021-08-29 12:20:12 +0200

    end work with package

--- src/mx/com/globalmobitoring/Props.java
+++ src/mx/com/globalmobitoring/Props.java
@@ -1,5 +1,7 @@
 package mx.com.globalmobitoring;
 
 public class Props {
-
+     public static void print(String s) {
+    	 System.out.println("s= "+s);
+     }
 }
---
+++ src/testPackage/Test.java
@@ -0,0 +1,13 @@
+package testPackage;
+
+//import mx.com.globalmobitoring.Props;
+//import static mx.com.globalmobitoring.Props.print;
+
+public class Test {
+	
+  public static void main(String[] args) {
+	//Props.print("hello");
+	  //print("hello there");
+	  mx.com.globalmobitoring.Props.print("hi there");
+}
+}
