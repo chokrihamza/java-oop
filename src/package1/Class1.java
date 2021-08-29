4d27b374fb3535c0a114b2379806436eb6ebe5f2
chokrihamza <chokrihamza1994@gmail.com>
2021-08-29 15:42:09 +0200

    operrateur

---
+++ src/package1/Class1.java
@@ -0,0 +1,16 @@
+package package1;
+
+public class Class1 {
+  protected String class1="class number one";
+  protected Class1(){
+	  System.out.println("protected constructor");
+  }
+  public Class1(String arg){
+	  System.out.println("public constructor");
+  }
+
+  
+  protected void methodclass1public() {
+	  System.out.println("i am Public method");
+  }
+}
---
+++ src/testmodificateur/test.java
@@ -0,0 +1,11 @@
+package testmodificateur;
+
+import package1.Class1;
+
+public class test {
+   public static void main(String[] args) {
+	Class1 class1=new Class1("dfg");
+	//System.err.println(class1.class1);
+	//class1.methodclass1public();
+}
+}
